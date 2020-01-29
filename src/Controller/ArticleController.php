791445a8ff5dce077da2b27d5467d61c00275644
mrybnik <rybnik.michal@gmail.com>
2020-01-29 14:05:45 +0100

    z6

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,13 +13,20 @@ class ArticleController extends AbstractController
     /**
      * @Route("/article/{name}", name="article")
      */
-    public function index($name)
+    public function index($name, Request $request)
     {
         if ($name == '404')
 //            throw $this->createNotFoundException();
             return new Response('404 not found !',Response::HTTP_NOT_FOUND);
 
+        $session = $request->getSession();
+
+        $counter = $session->get('counter_' . $name, 0);
+
+        $session->set('counter_' . $name, ++$counter);
+
+
         $backLink = "<a href='".$this->generateUrl('articles')."'>Back to list</a>";
-        return new Response('<html><body><h1>'.$name.'</h1><p>'.$backLink.'</p></body></html>');
+        return new Response('<html><body><h1>'.$name.'</h1><p>'.$backLink.'</p><p>Wyświetlono: ' . $counter . '</p></body></html>');
     }
 }
