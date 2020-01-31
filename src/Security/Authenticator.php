29339cc2d038a96ac06745cf3267555e4facc210
mrybnik <rybnik.michal@gmail.com>
2020-01-31 12:02:05 +0100

    Fix

--- src/Security/Authenticator.php
+++ src/Security/Authenticator.php
@@ -94,8 +94,7 @@ class Authenticator extends AbstractFormLoginAuthenticator implements PasswordAu
             return new RedirectResponse($targetPath);
         }
 
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('articles'));
     }
 
     protected function getLoginUrl()
